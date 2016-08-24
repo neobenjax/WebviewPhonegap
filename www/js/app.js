d256ef62824948635679ec972270dc1fecb87ad4
CodiceDeploy <jose.cruz@codice.com>
2016-08-24 16:00:08 -0500

    No status bar solo para ios

--- www/js/app.js
+++ www/js/app.js
@@ -131,7 +131,9 @@ var app = {
         }
     },
     onDeviceReady: function() {
-        StatusBar.overlaysWebView(false);
+        if( (navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) )
+            StatusBar.overlaysWebView(false);
+        
         var version = JSON.parse(localStorage.getItem('version'));
 
         if(version==null){
